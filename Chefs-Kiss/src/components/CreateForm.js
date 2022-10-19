8d918d4e6c915cce1ca4e0d264edf3dfba2391a4
Daniel <dbaray2118@gmail.com>
2022-10-19 10:04:22 -0700

    create a recipe form

--- Chefs-Kiss/src/components/CreateForm.js
+++ Chefs-Kiss/src/components/CreateForm.js
@@ -1,11 +1,29 @@
 function CreateForm() {
   return (
-    <form class="form-items" method="POST" action="">
-      <label htmlFor="name">Name:</label>
-      <input id="name" name="name" required />
-    
-      <label htmlFor="name">Instructions:</label>
-      <textarea rows="4" cols="50" id="instructions" name="instructions" required />
+    <form class="form-items" method="POST" action="/pages">
+      <label htmlFor="dish_Name">Dish Name:</label>
+      <input class="FormInputs" id="dish_Name" name="dish_Name" required />
+
+      <label htmlFor="picture">Picture:</label>
+      <input class="FormInputs" id="picture" name="picture" required />
+
+      <label for="meal">Meal of the day: idk if thats what its called</label>
+      <select class="FormInputs" id="meal" name="meal">
+        <option value="breakfast">Breakfast</option>
+        <option value="lunch">Lunch</option>
+        <option value="dinner">Dinner</option>
+      </select>
+
+      <label htmlFor="IngridientDirect">Ingridients/Directions:</label>
+      <textarea
+        rows="13"
+        class="FormInputs"
+        id="IngridientDirect"
+        name="IngridientDirect"
+        required
+      />
+      
+      <input type="submit" value="Add Dish" />
     </form>
   );
 }
