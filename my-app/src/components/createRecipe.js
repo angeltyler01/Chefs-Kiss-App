bb10b9bf9dda797a34ea5eb837fa912a6f25061b
Machew115 <why.mherrera@gmail.com>
2022-10-28 15:41:35 -0700

    still fixing

--- my-app/src/components/createRecipe.js
+++ my-app/src/components/createRecipe.js
@@ -3,7 +3,8 @@ import Form from './Formcreate'
 function createRecipe() {
     return (
         <div className='create-Recipe-page'>
-            <h1>share your recipes!</h1>
+            <h1 className="create-title">Share Your Recipes!</h1>
+            <hr className="horizontal-line"></hr>
             <Form />
         </div>
     )
