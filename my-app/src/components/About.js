7b097158782f068c8420427e0ab11a0bf54db369
Machew115 <why.mherrera@gmail.com>
2022-10-28 18:47:12 -0700

    added about us

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -5,6 +5,7 @@ function About() {
     return (
         <div className="about-container">
             <div className="about-discription">
+                <h1>About The Project</h1>
             <p>During the project, our team decided to construct a recipe website where users could create, edit and view their recipes. 
                 We had several ideas and features we wanted to add, such as a rating system with stars and the ability to comment on other users' recipes. 
                 Still, due to time constraints, we focused on getting a finished product out before features.
